4a046d39a72191bc430b026c8aec96bc0f1dc847
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-08-03 13:11:46 +0500

    fix: bug in the chekBox

--- src/components/elements/CheckBox.jsx
+++ src/components/elements/CheckBox.jsx
@@ -1,15 +1,15 @@
-export default function CheckBox({ title, onChange, value }) {
+export default function CheckBox({ title, className, onChange, value, yes = "Да", no = "Нет" }) {
   return (
-    <div className="check-box">
+    <div className={`check-box ${className}`}>
       <h1>{title}</h1>
       <div>
         <label onClick={() => onChange(true)}>
           <span className={value && "checked"}></span>
-          Да
+          {yes}
         </label>
         <label onClick={() => onChange(false)}>
           <span className={!value && "checked"}></span>
-          Нет
+          {no}
         </label>
       </div>
     </div>
