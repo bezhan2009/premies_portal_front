c2421dcb930c518d9197a632d62c4814c20d3622
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-08-26 16:08:15 +0500

    fix: bug for image display

--- src/components/elements/File.jsx
+++ src/components/elements/File.jsx
@@ -1,63 +1,79 @@
-import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 export default function FileUpload({
-                                       error,
-                                       id,
-                                       width = 20,
-                                       onChange,
-                                       placeholder,
-                                       placeholderImage,
-                                       value = null,
-                                   }) {
-    const [preview, setPreview] = useState(null);
+  error,
+  id,
+  width = 20,
+  onChange,
+  placeholder,
+  placeholderImage,
+  value,
+}) {
+  const [preview, setPreview] = useState(null);
+  const renderFileIcon = (path) => {
+    console.log("path", path);
 
-    useEffect(() => {
-        if (value instanceof File) {
-            const objectUrl = URL.createObjectURL(value);
-            setPreview(objectUrl);
+    if (path) {
+      const backendUrl = import.meta.env.VITE_BACKEND_APPLICATION_URL;
+      return `${backendUrl}/${path.replace(/\\/g, "/")}`;
+    } else {
+      return null;
+    }
+  };
+  console.log("value " + id , value);
+  
 
-            return () => URL.revokeObjectURL(objectUrl); // очистка при размонтировании
-        } else {
-            setPreview(null);
-        }
-    }, [value]);
+  useEffect(() => {
+    if (typeof value === "string") {
+      renderFileIcon(value);
+    } else {
+      if (value instanceof File) {
+        const objectUrl = URL.createObjectURL(value);
+        setPreview(objectUrl);
 
-    return (
-        <label className="file" htmlFor={id}>
-            <img
-                src={preview || placeholderImage}
-                width={width}
-                alt={placeholder || "Upload file"}
-            />
-            <input
-                id={id}
-                type="file"
-                placeholder={placeholder}
-                onChange={(e) => {
-                    const file = e.target.files?.[0];
-                    if (file) {
-                        onChange(file); // сохраняем именно File
-                    }
-                }}
-                accept="image/*"
-                aria-describedby={`${id}-error`}
-            />
-            {error?.[id] && (
-                <p id={`${id}-error`} className="error-input">
-                    {error[id]}
-                </p>
-            )}
-        </label>
-    );
+        return () => URL.revokeObjectURL(objectUrl); // очистка при размонтировании
+      } else {
+        setPreview(null);
+      }
+    }
+  }, [value]);
+
+  return (
+    <label className="file" htmlFor={id}>
+      <img
+        src={preview || placeholderImage}
+        width={width}
+        alt={placeholder || "Upload file"}
+      />
+      <input
+        id={id}
+        type="file"
+        placeholder={placeholder}
+        onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (file) {
+            onChange(file); // сохраняем именно File
+          }
+        }}
+        accept="image/*"
+        aria-describedby={`${id}-error`}
+      />
+      {error?.[id] && (
+        <p id={`${id}-error`} className="error-input">
+          {error[id]}
+        </p>
+      )}
+    </label>
+  );
 }
 
 FileUpload.propTypes = {
-    error: PropTypes.object,
-    id: PropTypes.string.isRequired,
-    width: PropTypes.number,
-    onChange: PropTypes.func.isRequired,
-    placeholder: PropTypes.string,
-    placeholderImage: PropTypes.string.isRequired,
-    value: PropTypes.any,
+  error: PropTypes.object,
+  id: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  placeholderImage: PropTypes.string.isRequired,
+  value: PropTypes.any,
 };
