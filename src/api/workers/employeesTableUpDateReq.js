791f6e658cb782f424d0f9c4f0091f9902ae2f32
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-08-31 23:51:15 +0500

    create: edit full data Office name in the «/operator/data» route №2

---
+++ src/api/workers/employeesTableUpDateReq.js
@@ -0,0 +1,12 @@
+import { apiClient } from "../utils/apiClient";
+
+export const employeesTableUpDateReq = async (data) => {
+  try {
+    const res = await apiClient.patch(`/workers/user/${data.ID}`, {
+      user: data,
+    });
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
--- src/components/dashboard/dashboard_operator/table_datas/TableEmployees.jsx
+++ src/components/dashboard/dashboard_operator/table_datas/TableEmployees.jsx
@@ -4,11 +4,14 @@ import Spinner from "../../../Spinner.jsx";
 import SearchBar from "../../../general/SearchBar.jsx";
 import { fetchOffices } from "../../../../api/offices/all_offices.js";
 import { translate_role_id } from "../../../../api/utils/translate_role_id.js";
+import { employeesTableUpDateReq } from "../../../../api/workers/employeesTableUpDateReq.js";
+import Input from "../../../elements/Input.jsx";
 
 const EmployeesTable = () => {
   const [employees, setEmployees] = useState([]);
   const [filteredEmployees, setFilteredEmployees] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [edit, setEdit] = useState(null);
 
   useEffect(() => {
     const loadData = async () => {
@@ -21,6 +24,7 @@ const EmployeesTable = () => {
           if (office.office_user && office.office_user.length > 0) {
             office.office_user.forEach((u) => {
               users.push({
+                ID: u.ID,
                 officeTitle: office.title,
                 fio: u.worker?.user?.full_name || "",
                 login: u.worker?.user?.Username || "",
@@ -43,9 +47,24 @@ const EmployeesTable = () => {
     loadData();
   }, []);
 
+  const handleChange = (key, value) => {
+    setEdit({ ...edit, [key]: value });
+  };
+
+  const saveChange = async () => {
+    try {
+      await employeesTableUpDateReq(edit);
+      setEdit({ ID: null });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   const handleSearch = (filtered) => {
     setFilteredEmployees(filtered || []);
   };
+
+  console.log("filteredEmployees", filteredEmployees);
 
   return (
     <div className="report-table-container">
@@ -58,12 +77,16 @@ const EmployeesTable = () => {
           (item) => item.officeTitle || "",
         ]}
       />
-      <div className="table-reports-div">
-        {loading ? (
-          <Spinner />
-        ) : (
-          <>
-            {filteredEmployees && filteredEmployees.length > 0 ? (
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {filteredEmployees && filteredEmployees.length > 0 ? (
+            <div
+              className="table-reports-div"
+              style={{ maxHeight: " calc(100vh - 480px)" }}
+            >
               <table className="table-reports">
                 <thead>
                   <tr>
@@ -79,23 +102,107 @@ const EmployeesTable = () => {
                 <tbody>
                   {filteredEmployees.map((row, idx) => (
                     <tr key={idx}>
-                      <td>{row.officeTitle}</td>
-                      <td>{row.fio}</td>
-                      <td>{row.login}</td>
-                      <td>{row.position}</td>
-                      <td>{row.placeWork}</td>
-                      <td>{row.salary}</td>
-                      <td>{row.group}</td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.officeTitle || row.officeTitle}
+                            type="text"
+                            value={edit?.officeTitle}
+                            onChange={(e) => handleChange("officeTitle", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.officeTitle
+                        )}
+                      </td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.fio || row.fio}
+                            type="text"
+                            value={edit?.fio}
+                            onChange={(e) => handleChange("fio", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.fio
+                        )}
+                      </td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.login || row.login}
+                            type="text"
+                            value={edit?.login}
+                            onChange={(e) => handleChange("login", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.login
+                        )}
+                      </td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.position || row.position}
+                            type="text"
+                            value={edit?.position}
+                            onChange={(e) => handleChange("position", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.position
+                        )}
+                      </td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.placeWork || row.placeWork}
+                            type="text"
+                            value={edit?.placeWork}
+                            onChange={(e) => handleChange("placeWork", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.placeWork
+                        )}
+                      </td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.salary || row.salary}
+                            type="text"
+                            value={edit?.salary}
+                            onChange={(e) => handleChange("salary", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.salary
+                        )}
+                      </td>
+                      <td onClick={() => setEdit(row)}>
+                        {edit?.ID === row.ID ? (
+                          <Input
+                            defValue={edit?.group || row.group}
+                            type="text"
+                            value={edit?.group}
+                            onChange={(e) => handleChange("group", e)}
+                            onEnter={() => saveChange(edit)}
+                          />
+                        ) : (
+                          row.group
+                        )}
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
-            ) : (
-              <h2>Нет данных</h2>
-            )}
-          </>
-        )}
-      </div>
+            </div>
+          ) : (
+            <h2>Нет данных</h2>
+          )}
+        </>
+      )}
     </div>
   );
 };
