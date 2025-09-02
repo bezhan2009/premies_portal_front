8d57affc8f373ca9d765d8159594d1530630ce69
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-09-02 16:26:06 +0500

    edit: fullUpdateWorkers

--- src/api/workers/fullUpdateWorkers.js
+++ src/api/workers/fullUpdateWorkers.js
@@ -2,14 +2,7 @@ import { apiClient } from "../utils/apiClient";
 
 export const fullUpdateWorkers = async (data, userObjact) => {
   try {
-    const res = await apiClient.patch(
-      `/workers/user/${data.ID}`,
-      userObjact
-        ? {
-            user: data,
-          }
-        : data
-    );
+    const res = await apiClient.patch(`/workers/user/${data.ID}`, data);
     return res.data;
   } catch (e) {
     console.error(e);
