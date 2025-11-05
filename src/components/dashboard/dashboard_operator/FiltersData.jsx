568004b0b446ae81d0d9b495958c2a328ebc6875
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-11-05 11:23:16 +0500

    add: new sectoion for margents

--- src/components/dashboard/dashboard_operator/FiltersData.jsx
+++ src/components/dashboard/dashboard_operator/FiltersData.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import '../../../styles/components/Filters.scss';
+import React from "react";
+import "../../../styles/components/Filters.scss";
 
 const FiltersDatas = ({ onSelect }) => {
   const buttons = [
-    { text: 'Отделения', class: 'reports_filter__button--grey', key: 'office' },
-    { text: 'Сотрудники', class: 'reports_filter__button--purple', key: 'employees' },
-    { text: 'Роли', class: 'reports_filter__button--yellow', key: 'roles' },
-    { text: 'Цены продуктов', class: 'reports_filter__button--cyan', key: 'prices' },
+    { text: "Отделения", class: "reports_filter__button--grey", key: "office" },
+    {
+      text: "Сотрудники",
+      class: "reports_filter__button--purple",
+      key: "employees",
+    },
+    { text: "Роли", class: "reports_filter__button--yellow", key: "roles" },
+    {
+      text: "Цены продуктов",
+      class: "reports_filter__button--cyan",
+      key: "prices",
+    },
+    { text: "Мерчанты", class: "reports_filter__button--grey", key: "margents" },
   ];
 
   return (
