bdd96d42ba78bd525e121cad1a6f5d42647cb86f
bezhan2009 <karimovbezan0@gmail.com>
2025-11-20 23:58:31 +0300

    added TechnicalDayBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import AppRouter from "./router";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import TechnicalDayBanner from "./pages/general/TechnicalDate.jsx";
 
 function App() {
+  const isTechnicalDay = true; 
+  if (isTechnicalDay === true) {
+    return <TechnicalDayBanner />;
+  }
+
   return <AppRouter />; 
 }
 
