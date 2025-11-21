5ad6ccbad90300340e92a8ef8a2a500e9983ed4e
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-11-21 22:36:44 +0500

    feat: Add V2 token translation API and implement token version checking in the router and login page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 function App() {
-  return <AppRouter />; 
+  return <AppRouter />;
 }
 
 export default App;
