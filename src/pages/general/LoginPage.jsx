41e6af9927e3efd599159b77da3d46b194716284
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-11-21 22:37:04 +0500

    feat: Add login page, configure application routing with authentication and role-based access, and introduce token version checking.

--- src/pages/general/LoginPage.jsx
+++ src/pages/general/LoginPage.jsx
@@ -1,46 +1,52 @@
-import { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import '../../styles/login.scss';
-import LogoImageComponent from '../../components/Logo';
-import Spinner from '../../components/Spinner';
-import { login } from '../../api/auth';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Helmet } from 'react-helmet';
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../../styles/login.scss";
+import LogoImageComponent from "../../components/Logo";
+import Spinner from "../../components/Spinner";
+import { login } from "../../api/auth";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Helmet } from "react-helmet";
+import { getV2Token } from "../../api/getV2Token";
 
 export default function LoginPage() {
-  const [username, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
   const location = useLocation();
 
-  const from = location.state?.from?.pathname || '/';
+  const from = location.state?.from?.pathname || "/";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     setLoading(true);
 
     try {
       const data = await login(username, password);
-      console.log('Logged in:', data);
+      console.log("Logged in:", data);
+      const v2Token = await getV2Token({ token: data.access_token });
 
-      localStorage.setItem('access_token', data.access_token);
-      localStorage.setItem('refresh_token', data.refresh_token);
+      console.log("v2Token", v2Token);
 
-      localStorage.setItem('username', username);
+      if (!v2Token) {
+        localStorage.setItem("access_token", data.access_token);
+        localStorage.setItem("refresh_token", data.refresh_token);
+        localStorage.setItem("username", username);
+      }
+
 
       if (data.role_id !== undefined) {
-        localStorage.setItem('role_id', String(data.role_id));
+        localStorage.setItem("role_id", String(data.role_id));
       }
 
       navigate(from, { replace: true });
     } catch (err) {
       if (err.message === "Failed to fetch") {
-        setError('Сервис сейчас недоступен');
+        setError("Сервис сейчас недоступен");
       } else {
         setError(err.message);
       }
@@ -54,56 +60,60 @@ export default function LoginPage() {
   };
 
   return (
-      <>
-        <Helmet>
-          <title>Логин</title>
-        </Helmet>
-        <div className="login-container">
-          <form className="login-form" onSubmit={handleSubmit}>
-            <div align="center" className='image-logo-login'>
-              <LogoImageComponent width={125} height={105} />
-            </div>
+    <>
+      <Helmet>
+        <title>Логин</title>
+      </Helmet>
+      <div className="login-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div align="center" className="image-logo-login">
+            <LogoImageComponent width={125} height={105} />
+          </div>
 
-            <label>
-              <input
-                  type="text"
-                  value={username}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder='Логин'
-                  required
-              />
-            </label>
+          <label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Логин"
+              required
+            />
+          </label>
 
-            <label style={{ position: 'relative' }}>
-              <input
-                  type={showPassword ? 'text' : 'password'}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder='Пароль'
-                  required
-              />
-              <button
-                  type="button"
-                  className='toggle-password-visibility'
-                  onClick={togglePasswordVisibility}
-              >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </button>
-            </label>
-
-            {error && <div align="center" className="error">{error}</div>}
-
-            <button type="submit" disabled={loading}>
-              {loading ? (
-                  <div align="center">
-                    <Spinner />
-                  </div>
-              ) : (
-                  'Войти'
-              )}
+          <label style={{ position: "relative" }}>
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Пароль"
+              required
+            />
+            <button
+              type="button"
+              className="toggle-password-visibility"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
             </button>
-          </form>
-        </div>
-      </>
+          </label>
+
+          {error && (
+            <div align="center" className="error">
+              {error}
+            </div>
+          )}
+
+          <button type="submit" disabled={loading}>
+            {loading ? (
+              <div align="center">
+                <Spinner />
+              </div>
+            ) : (
+              "Войти"
+            )}
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
