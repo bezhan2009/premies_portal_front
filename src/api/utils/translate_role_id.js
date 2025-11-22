9c551f05ccf16d421151fce2b5f7976ed6f4eec1
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-11-22 09:02:01 +0500

    feat: Add user authentication with login page, role-based dashboard redirection, and a searchable employee data table with editing capabilities.

--- src/api/utils/translate_role_id.js
+++ src/api/utils/translate_role_id.js
@@ -1,9 +1,9 @@
-export function translate_role_id(role_id) {
-    if (role_id === 6) {
-        return "Корт";
-    } else if (role_id === 8) {
-        return "Карзхо ва дигар"
-    } else {
-        return "undefined"
-    }
+export function translate_role_id(role_ids) {
+  if (role_ids === 6) {
+    return "Корт";
+  } else if (role_ids === 8) {
+    return "Карзхо ва дигар";
+  } else {
+    return "undefined";
+  }
 }
