5a3181a7b92441c814f1d1ee4274f880fd7b4dea
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-12-15 12:51:07 +0500

    fix: format and clean up transaction API functions for consistency

--- src/api/transactions/api.js
+++ src/api/transactions/api.js
@@ -1,47 +1,45 @@
 import { apiClientTransactions } from "../utils/apiClientTransactions";
 
-export const putTransactions = async ( data) => {
-         try {
-            const response = await apiClientTransactions.put(
-                "/api/Transactions/transaction-type/update",
-                {
-                    ...data,
-                    number: +data?.number,
-                    type: +data?.type
-                }
-            );
-            return response;
-        } catch (e) {
-            // const errorMessage = e?.response?.data?.message || e?.message || "Произошла ошибка при обновлении";
-            console.error("Ошибка при обновлении:", e);
-        } 
-}
+export const putTransactions = async (data) => {
+  try {
+    const response = await apiClientTransactions.put(
+      "/api/Transactions/transaction-type/update",
+      {
+        ...data,
+        number: +data?.number,
+        type: +data?.type,
+      }
+    );
+    return response;
+  } catch (e) {
+    console.error("Ошибка при обновлении:", e);
+  }
+};
 
-
-export const putTransactionsNumber = async ( data) => {
-         try {
-            const response = await apiClientTransactions.put(
-                "/api/Transactions/transaction-type/update-number",
-                {
-                    ...data,
-                    newNumber: +data?.number,
-                    type: +data?.type
-                }
-            );
-            return response;
-        } catch (e) {
-            // const errorMessage = e?.response?.data?.message || e?.message || "Произошла ошибка при обновлении";
-            console.error("Ошибка при обновлении:", e);
-        } 
-}
+export const putTransactionsNumber = async (data) => {
+  try {
+    const response = await apiClientTransactions.put(
+      "/api/Transactions/transaction-type/update-number",
+      {
+        ...data,
+        newNumber: +data?.number,
+        type: +data?.type,
+      }
+    );
+    return response;
+  } catch (e) {
+    // const errorMessage = e?.response?.data?.message || e?.message || "Произошла ошибка при обновлении";
+    console.error("Ошибка при обновлении:", e);
+  }
+};
 
 export const getTransactions = async () => {
-         try {
-            const response = await apiClientTransactions.get(
-                "/api/Transactions/types/all");
-            return response;
-        } catch (e) {
-            console.error("Ошибка при обновлении:", e);
-        }
-}
-
+  try {
+    const response = await apiClientTransactions.get(
+      "/api/Transactions/types/all"
+    );
+    return response;
+  } catch (e) {
+    console.error("Ошибка при обновлении:", e);
+  }
+};
