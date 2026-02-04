8da0bbd6ea71ea561cb9c06ba9fb981a43753c49
Kastro <boboevdjasur@gmail.com>
2026-02-04 15:07:05 +0500

    returned App render

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.scss';
-import AtmStickyTable from './features/atm-table/atm-table';
-import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <AtmStickyTable />
-    </BrowserRouter>
+    <App />
 );
