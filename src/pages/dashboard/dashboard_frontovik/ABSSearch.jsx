01242e50f4e0e8cf2aa0aa9d381b070201b56caf
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-15 22:00:05 +0500

    Remove unused Header and GetBlockInfo imports and components from the ABSSearch page.

--- src/pages/dashboard/dashboard_frontovik/ABSSearch.jsx
+++ src/pages/dashboard/dashboard_frontovik/ABSSearch.jsx
@@ -1,9 +1,7 @@
-import '../../../styles/dashboard.scss';
-import Header from '../../../components/dashboard/dashboard_operator/MenuOperator';
-import GetBlockInfo from '../../../components/general/GeneralBlockInfo.jsx';
-import { Helmet } from 'react-helmet';
-import useSidebar from '../../../hooks/useSideBar.js';
-import Sidebar from '../../../components/general/DynamicMenu.jsx';
+import "../../../styles/dashboard.scss";
+import { Helmet } from "react-helmet";
+import useSidebar from "../../../hooks/useSideBar.js";
+import Sidebar from "../../../components/general/DynamicMenu.jsx";
 import ABSClientSearch from "../../../components/dashboard/dashboard_frontovik/ABSSearch.jsx";
 
 export default function DashboardFrontovikAbsSearch() {
@@ -14,14 +12,19 @@ export default function DashboardFrontovikAbsSearch() {
       <Helmet>
         <title>Поиск по АБС</title>
       </Helmet>
-      <div className={`dashboard-container ${isSidebarOpen ? 'sidebar-open' : 'sidebar-collapsed'}`}>
-          <Sidebar activeLink="abs_search" isOpen={isSidebarOpen} toggle={toggleSidebar} />
+      <div
+        className={`dashboard-container ${isSidebarOpen ? "sidebar-open" : "sidebar-collapsed"}`}
+      >
+        <Sidebar
+          activeLink="abs_search"
+          isOpen={isSidebarOpen}
+          toggle={toggleSidebar}
+        />
 
-          <div className="dashboard-container">
-            <ABSClientSearch />
-          </div>
-
+        <div className="dashboard-container">
+          <ABSClientSearch />
         </div>
+      </div>
     </>
   );
 }
